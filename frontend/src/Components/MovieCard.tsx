d77556763498890292cf38d4d83431db65609f9d
Artur <arturbrnd1@gmail.com>
2026-05-12 14:28:33 -0300

    fix: tailwindCSS class

--- frontend/src/Components/MovieCard.tsx
+++ frontend/src/Components/MovieCard.tsx
@@ -11,14 +11,14 @@ function MovieCard({ movie }: MovieCardProps) {
 
   return (
     <div className="group relative rounded-lg overflow-hidden bg-[#1a1a1a] transition-transform duration-200 flex flex-col     text-[0.9rem] md:text-base hover:-translate-y-1">
-      <div className="relative aspect-[2/3] w-full">
+      <div className="relative aspect-2/3 w-full">
         <img
           className="h-full w-full object-cover"
           src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
           alt={movie.title}
         />
 
-        <div className="absolute inset-0 bg-gradient-to-b from-black/10 to-black/80 opacity-0 transition-opacity duration-200 flex flex-col justify-end p-4 group-hover:opacity-100">
+        <div className="absolute inset-0 bg-linear-to-b from-black/10 to-black/80 opacity-0 transition-opacity duration-200 flex flex-col justify-end p-4 group-hover:opacity-100">
           <button
             className="absolute top-4 right-4 text-white text-[1.2rem] md:text-2xl p-2 bg-black/50 rounded-full w-8 h-8 md:w-10 md:h-10 flex items-center justify-center transition-colors duration-200 hover:bg-black/80"
             onClick={handleFavMovie}
